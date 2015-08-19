9f962993c2adf8350b7a85c58d715829a290e4df
Paco Contreras <pacocontrerasdct@gmail.com>
2015-08-19 21:37:37 +0100

    Passing value from input in deposit1 to balance1 and printing into div Current Account

--- atm-start_jQuery/atm.js
+++ atm-start_jQuery/atm.js
@@ -1,4 +1,54 @@
 $(document).ready(function(){
-  
-})
+  console.log('Ready!!!');
 
+
+
+
+
+
+
+function changeBackgroundColor(){
+  backColor = $('div#balance1.balance')[0];
+  console.log(backColor);
+  $(backColor).css({'background-color':'red'});
+}
+
+function getDepositAmount(){
+ deposit1 = $('input#deposit1');
+ console.log(deposit1);
+   $(deposit1).on('click', function(e) {
+    console.log('Hello deposit 1');
+    amount1 = $('input#amount1');
+    value1 = amount1[0].value;
+    console.log(value1);
+    
+    idBalance = $('div#balance1')[0];
+    idBalance = $(idBalance).html('$' + value1);
+    
+
+    //balance1 = $('input#balance1');
+    //balance = balance1[0].outerText;
+    console.log(idBalance);
+    });
+}
+getDepositAmount();
+
+
+function getWithdrawAmount(){
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}) // Closing brackets and curly brackets from ready function in first line.
